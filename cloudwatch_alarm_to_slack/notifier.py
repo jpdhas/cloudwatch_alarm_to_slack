6e1840aa2fcb55d78ad59ab93c0e6d201138c7e2
Jermila Paul Dhas <jpauldhas@expediagroup.com>
2020-11-01 02:40:25 +0000

    Exception handling in the notifier and unit tests.

--- cloudwatch_alarm_to_slack/notifier.py
+++ cloudwatch_alarm_to_slack/notifier.py
@@ -2,6 +2,7 @@
 import json
 
 import cloudwatch_alarm_to_slack.utils.log as log
+from cloudwatch_alarm_to_slack.errors import MalformedSnsPayloadError
 from cloudwatch_alarm_to_slack.events.cloudwatch import CloudwatchAlarm
 
 LOGGER = log.custom_logger(__name__)
@@ -9,7 +10,10 @@ LOGGER = log.custom_logger(__name__)
 
 def handle_event(event):
     """Receive Cloudwatch alarm notification from SNS."""
-    message = json.loads(event['Records'][0]['Sns']['Message'])
+    try:
+        message = json.loads(event['Records'][0]['Sns']['Message'])
 
-    LOGGER.info('Processing event: %s', message)
-    CloudwatchAlarm.process_alarm(alarm=message)
+        LOGGER.info('Processing event: %s', message)
+        CloudwatchAlarm.process_alarm(alarm=message)
+    except (IndexError, KeyError) as e:
+        raise MalformedSnsPayloadError(e)
--- tests/test_notifier.py
+++ tests/test_notifier.py
@@ -2,6 +2,9 @@
 
 from unittest import mock
 
+import pytest
+
+from cloudwatch_alarm_to_slack.errors import MalformedSnsPayloadError
 from cloudwatch_alarm_to_slack.notifier import handle_event
 
 
@@ -41,3 +44,38 @@ def test_cloudwatch_alarm_process_alarm(mock_cloudwatch_alarm):
         }
     }
     mock_cloudwatch_alarm.assert_called_once_with(alarm=mock_message)
+
+
+@mock.patch('cloudwatch_alarm_to_slack.events.cloudwatch.CloudwatchAlarm.process_alarm')
+def test_cloudwatch_alarm_process_alarm_custom_exception_keyerror(mock_cloudwatch_alarm):
+    """Test if notifier throws custom exception for invalid payload."""
+
+    mock_cloudwatch_event = {
+        'Records': [
+            {
+                'Sns': {
+                    'Type': 'Notification',
+                    'Subject': 'test_subject',
+                }
+            }
+        ]
+    }
+
+    with pytest.raises(MalformedSnsPayloadError):
+        handle_event(mock_cloudwatch_event)
+
+    mock_cloudwatch_alarm.assert_not_called()
+
+
+@mock.patch('cloudwatch_alarm_to_slack.events.cloudwatch.CloudwatchAlarm.process_alarm')
+def test_cloudwatch_alarm_process_alarm_custom_exception_indexerror(mock_cloudwatch_alarm):
+    """Test if notifier throws custom exception for invalid payload."""
+
+    mock_cloudwatch_event = {
+        'Records': []
+    }
+
+    with pytest.raises(MalformedSnsPayloadError):
+        handle_event(mock_cloudwatch_event)
+
+    mock_cloudwatch_alarm.assert_not_called()
