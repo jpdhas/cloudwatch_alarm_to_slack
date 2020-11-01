e57ad6ee26ad9edf91893b97922ef4bd7057d148
Jermila Paul Dhas <jpauldhas@expediagroup.com>
2020-11-01 23:35:01 +0000

    Add more metadata for the library

--- setup.py
+++ setup.py
@@ -4,16 +4,26 @@
 
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
       author='Jermila Paul Dhas',
       author_email='dhas.jermila@gmail.com',
       python_requires='>=3.6',
       description='A python library to send cloudwatch alarm events from sns to slack',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       license='MIT license',
       keywords='cloudwatch_alarm_to_slack',
       name='cloudwatch_alarm_to_slack',
       version='0.1',
       url='http://github.com/storborg/funniest',
       packages=['cloudwatch_alarm_to_slack'],
+      classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+      ],
       zip_safe=False
 )
