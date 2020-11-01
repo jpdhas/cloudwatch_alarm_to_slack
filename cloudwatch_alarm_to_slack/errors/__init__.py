438fd687e8ba7ae967afadbd9ac55da744177799
Jermila Paul Dhas <jpauldhas@expediagroup.com>
2020-11-01 21:28:38 +0000

    Introduced a custom exception when environment variables are not set

--- cloudwatch_alarm_to_slack/errors/__init__.py
+++ cloudwatch_alarm_to_slack/errors/__init__.py
@@ -3,3 +3,7 @@
 
 class MalformedSnsPayloadError(Exception):
     """Error raised when the event payload is not a valid event."""
+
+
+class EnvironmentVariableNotFound(Exception):
+    """Error raised when the environment variables are not set."""
