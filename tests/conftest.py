fb5f46b8da42bdd46f43dab8c8c8f7cacbb8dce3
Jermila Paul Dhas <jpauldhas@expediagroup.com>
2020-11-01 01:33:25 +0000

    Unit test for the notifier entrypoint

---
+++ tests/conftest.py
@@ -0,0 +1,9 @@
+"""Pytest config file."""
+
+
+import pytest
+
+@pytest.fixture(autouse=True)
+def default_env_vars(monkeypatch):
+    monkeypatch.setenv('SLACK_BOT_TOKEN', 'test_slack_bot_token')
+    monkeypatch.setenv('SLACK_CHANNEL', 'test_slack_channel')
---
+++ tests/test_notifier.py
@@ -0,0 +1,43 @@
+"""Test for cloudwatch_alarm_to_slack/notifier."""
+
+from cloudwatch_alarm_to_slack.notifier import handle_event
+
+from unittest import mock
+
+
+@mock.patch('cloudwatch_alarm_to_slack.events.cloudwatch.CloudwatchAlarm.process_alarm')
+def test_cloudwatch_alarm_process_alarm(mock_cloudwatch_alarm):
+    """Test if cloudwatch alarm is processsed for a valid event."""
+
+    mock_cloudwatch_event = {
+        'Records': [
+            {
+                'Sns': {
+                    'Type': 'Notification',
+                    'Subject': 'test_subject',
+                    'Message': '{"AlarmName":"test_alarm","AlarmDescription":"test_description","AWSAccountId":"test_account","NewStateValue":"ALARM","Region":"US West (Oregon)","OldStateValue":"INSUFFICIENT_DATA","Trigger":{"MetricName":"Invocations","Namespace":"test_service","StatisticType":"Statistic","Statistic":"SUM","Period":60,"EvaluationPeriods":1,"ComparisonOperator":"LessThanOrEqualToThreshold","Threshold":0.0}}'
+                }
+            }
+        ]
+    }
+
+    handle_event(mock_cloudwatch_event)
+    mock_message = {
+        'AlarmName': 'test_alarm',
+        'AlarmDescription': 'test_description',
+        'AWSAccountId': 'test_account',
+        'NewStateValue': 'ALARM',
+        'Region': 'US West (Oregon)',
+        'OldStateValue': 'INSUFFICIENT_DATA',
+        'Trigger': {
+            'MetricName': 'Invocations',
+            'Namespace': 'test_service',
+            'StatisticType': 'Statistic',
+            'Statistic': 'SUM',
+            'Period': 60,
+            'EvaluationPeriods': 1,
+            'ComparisonOperator': 'LessThanOrEqualToThreshold',
+            'Threshold': 0.0
+        }
+    }
+    mock_cloudwatch_alarm.assert_called_once_with(alarm=mock_message)
